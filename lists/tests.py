094648da03e0dc47beaf64687afb48a4ed2bd326
sven0906 <sven-@hanmail.net>
2022-03-23 16:01:07 +0900

    ITEM 모델과 Migrations

--- lists/tests.py
+++ lists/tests.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.template.loader import render_to_string
 from lists.views import home_page
+from lists.models import Item
 
 
 class SmokeTest(TestCase):
@@ -50,3 +51,23 @@ class SmokeTest(TestCase):
         ))
         response_decode = self.remove_csrf(response.content.decode())
         self.assertEqual(response_decode, expected_html)
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = '첫 번째 아이템'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = '두 번째 아이템'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, '첫 번째 아이템')
+        self.assertEqual(second_saved_item.text, '두 번째 아이템')
