8ac6108f2e1b04924b0c5109e204a0a0484b017c
sven0906 <sven-@hanmail.net>
2022-03-23 15:41:58 +0900

    리펙터링 전

--- functional_tests.py
+++ functional_tests.py
@@ -20,35 +20,48 @@ class NewVisitorTest(unittest.TestCase):
         # assert 'To-do' in browser.title, "Brower title was " + browser.title
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
+        # header_text = self.browser.find_element(by='h1').text
         self.assertIn('To-Do', header_text)
 
         # 사용자A는 바로 작업을 추가한다.
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            '작업 아이템 입력'
-        )
+        inputbox.send_keys('공작깃털을 이용해서 그물 만들기')
+        # self.assertEqual(
+        #     inputbox.get_attribute('placeholder'),
+        #     '작업 아이템 입력'
+        # )
 
         # "공작깃털 사기"라고 텍스트 상자에 입력한다.
         # (에디스의 취미는 날치 잡이용 그물을 만드는 것이다)
-        inputbox.send_keys('공작깃털 사기')
+        # inputbox.send_keys('공작깃털 사기')
 
         # 엔터키를 치면 페이지가 갱신되고 작업목록에
         # "1: 공작깃털 사기" 아이템이 추가된다.
         inputbox.send_keys(Keys.ENTER)
 
+        import time
+        time.sleep(3)
+
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: 공작깃털 사기' for row in rows),
-            '신규 작업이 테이블에 표시되지 않는다'
-        )
+        # self.assertTrue(
+        #     any(row.text == '1: 공작깃털 사기' for row in rows),
+        #     '신규 작업이 테이블에 표시되지 않는다'
+        # )
+        self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다.
         # 다시 "공작깃털을 이용해서 그물 만들기"라고 입력한다.
-        self.fail('Finish the Test!')
+
 
         # 페이지는 다시 갱신되고, 두개 아이템이 목록에 보인다.
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
+        self.assertIn(
+            '2: 공작깃털을 이용해서 그물 만들기',
+            [row.text for row in rows]
+        )
         # 사용자A는 사이트가 입력한 목록을 저장하고 있는지 궁금하다
         # 사이트는 사용자A를 위해 특정 URL을 생성해준다
         # 이때 URL에 대한 설명도 함께 제공된다.
@@ -56,6 +69,8 @@ class NewVisitorTest(unittest.TestCase):
         # 해당 URL 에 접속하면 작업 목록이 그대로 있는 것을 확인할 수 있다.
 
         # browser.quit()
+
+        self.fail('Finish the Test!')
 
 
 if __name__ == '__main__':
--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
+import re
 from django.http import HttpRequest
 from django.test import TestCase
+from django.shortcuts import render
 # from django.core.urlresolvers import resolve ->
 from django.urls import reverse
 from django.template.loader import render_to_string
@@ -11,15 +13,40 @@ class SmokeTest(TestCase):
     # def test_bad_maths(self):
     #     self.assertEqual(1 + 1, 3)
 
+    def remove_csrf(self, origin):
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        return re.sub(csrf_regex, '', origin)
+
     # def test_root_url_resolves_to_home_page_view(self):
     #     found = reverse('/')
     #     self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest() # HttpRequest 객체를 생성한다.
-        response = home_page(request) # home_page 뷰에 전달해서 응답을 가져온다.
+        request = HttpRequest()  # HttpRequest 객체를 생성한다.
+
+        response = home_page(request)  # home_page 뷰에 전달해서 응답을 가져온다.
         # self.assertTrue(response.content.startswith(b'<html>'))
         # self.assertIn(b'<title>To-Do lists</title>', response.content)
         # self.assertTrue(response.content.endswith(b'</html>'))
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        expected_html = self.remove_csrf(render_to_string('home.html', request=request))
+        response_decode = self.remove_csrf(response.content.decode())
+        self.assertEqual(response_decode, expected_html)
+
+    def test_home_page_can_save_a_post_request(self):
+        """ 설정(Setup) """
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        """ 처리(Exercise) """
+        response = home_page(request)
+
+        """ 어설션(Assert) """
+        self.assertIn('신규 작업 아이템', response.content.decode())
+        expected_html = self.remove_csrf(render_to_string(
+            'home.html',
+            {'new_item_text': '신규 작업 아이템'},
+            request=request
+        ))
+        response_decode = self.remove_csrf(response.content.decode())
+        self.assertEqual(response_decode, expected_html)
