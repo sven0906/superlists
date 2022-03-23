9b5f79716aad09664c84ac5f7363178297d0fe7c
sven0906 <sven-@hanmail.net>
2022-03-23 17:21:12 +0900

    POST 후에 리디렉션과 템플릿에 모든 작업 아이템 표시

--- lists/tests.py
+++ lists/tests.py
@@ -11,9 +11,6 @@ from lists.models import Item
 
 class HomePageTest(TestCase):
 
-    # def test_bad_maths(self):
-    #     self.assertEqual(1 + 1, 3)
-
     def remove_csrf(self, origin):
         csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
         return re.sub(csrf_regex, '', origin)
@@ -24,11 +21,8 @@ class HomePageTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()  # HttpRequest 객체를 생성한다.
-
         response = home_page(request)  # home_page 뷰에 전달해서 응답을 가져온다.
-        # self.assertTrue(response.content.startswith(b'<html>'))
-        # self.assertIn(b'<title>To-Do lists</title>', response.content)
-        # self.assertTrue(response.content.endswith(b'</html>'))
+
         expected_html = self.remove_csrf(render_to_string('home.html', request=request))
         response_decode = self.remove_csrf(response.content.decode())
         self.assertEqual(response_decode, expected_html)
@@ -47,23 +41,30 @@ class HomePageTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, '신규 작업 아이템')
 
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        response = home_page(request)
+
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/')
-
-        # 더 이상 response.content가 템플릿에 의해 렌더링 되지 않음
-        # self.assertIn('신규 작업 아이템', response.content.decode())
-        # expected_html = self.remove_csrf(render_to_string(
-        #     'home.html',
-        #     {'new_item_text': '신규 작업 아이템'},
-        #     request=request
-        # ))
-        # response_decode = self.remove_csrf(response.content.decode())
-        # self.assertEqual(response_decode, expected_html)
 
     def test_home_page_only_saves_items_when_necessary(self):
         request = HttpRequest()
         home_page(request)
         self.assertEqual(Item.objects.count(), 0)
+
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
