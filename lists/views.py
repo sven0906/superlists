52d2692d6be966a0aad17f775d83f0235bd0cf23
sven0906 <sven-@hanmail.net>
2022-04-28 14:22:46 +0900

    기존 리스트에 아이템을 추가하기 위한 신규 URL 및 뷰
    FT 성공

--- lists/views.py
+++ lists/views.py
@@ -7,12 +7,18 @@ def home_page(request):
     return render(request, 'home.html')
 
 
-def view_list(request):
-    items = Item.objects.all()
-    return render(request, 'list.html', {'items': items})
+def view_list(request, id):
+    list_ = List.objects.get(id=id)
+    return render(request, 'list.html', {'list': list_})
 
 
 def new_list(request):
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/the-only-list-in-the-world/')
+    return redirect('/lists/%d/' % (list_.id,))
+
+
+def add_item(request, id):
+    list_ = List.objects.get(id=id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/%d/' % (list_.id,))
